7524070b84489ef373dd2ce0a3463eb5516c0242
wc503958469 <503958469@qq.com>
2015-10-09 08:54:13 +0800

    Initial Commit

---
+++ Nav/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nav
+//
+//  Created by wangchen on 15/10/9.
+//  Copyright © 2015年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
